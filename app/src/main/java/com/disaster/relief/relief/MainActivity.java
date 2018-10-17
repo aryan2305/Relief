0e7e5fb832e214c77c2af9286640335995c2814f
Sparsh Dutta <33557772+sparsh2706@users.noreply.github.com>
2018-10-17 13:34:41 +0530

    Update MainActivity.java

--- app/src/main/java/com/disaster/relief/relief/MainActivity.java
+++ app/src/main/java/com/disaster/relief/relief/MainActivity.java
@@ -1,4 +1,4 @@
-package com.disaster.relief.relief;
+package com.example.sparsh.myapplication;
 
 import android.Manifest;
 import android.content.Intent;
@@ -8,44 +8,55 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import static android.os.Build.VERSION;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = MainActivity.class.getSimpleName();
-    private TextView userDetails;
-    private Button SOS;
-
+    private static final String TAG = "MainActivity";
+    static String num;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final Button SOS;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Intent intent = getIntent();
-        String username = intent.getStringExtra("username");
-        if(username == null || username.isEmpty())
-        {
-            username = "User";
-        }
-
-        userDetails = (TextView) findViewById(R.id.textview_UserDetails);
-        userDetails.setText("Hey "+username +" !!");
-
-        SOS = findViewById(R.id.sos);
+        SOS = (Button)findViewById(R.id.sos);
 
         SOS.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
-                callPhoneNumber();
+                PopupMenu emergency_popup = new PopupMenu(MainActivity.this, SOS);
+                emergency_popup.getMenuInflater().inflate(R.menu.popup_emergency,emergency_popup.getMenu());
+                emergency_popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        String num = (String) item.getTitle();
+                        if(num.equals("HELPLINE"))
+                            callPhoneNumber("45791332123");
+                        else if(num.equals("FIRE"))
+                            callPhoneNumber("7896657602");
+                        else if(num.equals("HOSPITAL"))
+                            callPhoneNumber("123456798");
+                        else if(num.equals("POLICE"))
+                            callPhoneNumber(("9406964210"));
+
+                        Toast.makeText(MainActivity.this,"You Clicked : " + num, Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+                emergency_popup.show();
             }
         });
 
     }
 
-    public void callPhoneNumber()
+
+
+    public void callPhoneNumber(String num)
     {
         try
         {
@@ -60,13 +71,13 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 Intent callIntent = new Intent(Intent.ACTION_CALL);
-                callIntent.setData(Uri.parse("tel:" + "8399895789"));
+                callIntent.setData(Uri.parse("tel:" + num));
                 startActivity(callIntent);
 
             }
             else {
                 Intent callIntent = new Intent(Intent.ACTION_CALL);
-                callIntent.setData(Uri.parse("tel:" + "8399895789"));
+                callIntent.setData(Uri.parse("tel:" + num));
                 startActivity(callIntent);
             }
         }
@@ -84,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
         {
             if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
             {
-                callPhoneNumber();
+                callPhoneNumber(num);
             }
             else
             {
