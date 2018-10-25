fdea32656cddaabeb817af4739f0f1277c8db090
Aryan Agrawal <aryanpranshu@gmail.com>
2018-10-25 15:08:45 +0530

    some changes

---
+++ app/src/main/java/com/disaster/relief/relief/NewsFeedActivity.java
@@ -0,0 +1,71 @@
+package com.disaster.relief.relief;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class NewsFeedActivity extends AppCompatActivity {
+
+    private static final String TAG = "Json output";
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_feed);
+
+        mTextView = (TextView) findViewById(R.id.mTextView_layout);
+        String url = "https://eonet.sci.gsfc.nasa.gov/api/v2.1/categories/14?days=20";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG,response.toString());
+                        try{
+                            String title = response.getString("title");
+                            String description = response.getString("description");
+
+                            String jsonResponse = "Title" + title + "Description" + description;
+                            mTextView.setText(jsonResponse);
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                            Toast.makeText(getApplicationContext(),
+                                    "Error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        VolleyLog.d(TAG, "Error: " + error.getMessage());
+                        Toast.makeText(getApplicationContext(),
+                                error.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+// Access the RequestQueue through your singleton class.
+        AppSingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+    }
+
+}
